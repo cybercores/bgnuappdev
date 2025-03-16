569f9279ceddd74e208fe82409e08362a7bc15b6
cybercores <maheenafzal500@gmail.com>
2025-03-16 12:29:37 +0500

    One file Registraion
    in one file Registration and displaying data

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -9,26 +11,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AzeemShakirBSCSF22M08',
+      title: 'Registration Page',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: MainPage(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class MainPage extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    RegistrationPage(),
+    DataListPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    Navigator.pop(context); // Close the drawer
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First App'),
+        title: Text('Main Page'),
       ),
       drawer: Drawer(
         child: ListView(
@@ -47,148 +63,306 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             ListTile(
-              title: Text('Calculator'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CalculatorScreen(),
-                  ),
-                );
-              },
+              leading: Icon(Icons.app_registration),
+              title: Text('Registration Form'),
+              selected: _selectedIndex == 0,
+              onTap: () => _onItemTapped(0),
             ),
             ListTile(
-              title: Text('Grades Page'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => StudentDataGrid(),
-                  ),
-                );
-              },
+              leading: Icon(Icons.list),
+              title: Text('Saved Data'),
+              selected: _selectedIndex == 1,
+              onTap: () => _onItemTapped(1),
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Text('Welcome to the Home Screen!'),
-      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
 
-class CalculatorScreen extends StatefulWidget {
+class RegistrationPage extends StatefulWidget {
   @override
-  _CalculatorScreenState createState() => _CalculatorScreenState();
+  _RegistrationPageState createState() => _RegistrationPageState();
 }
 
-class _CalculatorScreenState extends State<CalculatorScreen> {
-  final TextEditingController _num1Controller = TextEditingController();
-  final TextEditingController _num2Controller = TextEditingController();
-  String _selectedOperation = 'Add';
-  String _result = '';
+class _RegistrationPageState extends State<RegistrationPage>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String _status = 'Active';
 
-  void _calculate() {
-    double num1 = double.tryParse(_num1Controller.text) ?? 0;
-    double num2 = double.tryParse(_num2Controller.text) ?? 0;
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
 
-    if (_num1Controller.text.isEmpty || _num2Controller.text.isEmpty) {
-      setState(() {
-        _result = 'Please enter both numbers.';
-      });
-      return;
-    }
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
 
-    double result = 0;
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
 
-    switch (_selectedOperation) {
-      case 'Add':
-        result = num1 + num2;
-        break;
-      case 'Subtract':
-        result = num1 - num2;
-        break;
-      case 'Multiply':
-        result = num1 * num2;
-        break;
-      case 'Divide':
-        result = num2 != 0 ? num1 / num2 : double.infinity;
-        break;
-    }
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
 
-    setState(() {
-      _result = 'Result: $result';
-    });
+    _controller.forward();
   }
 
   @override
   void dispose() {
-    _num1Controller.dispose();
-    _num2Controller.dispose();
+    _controller.dispose();
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
+  }
+
+  Future<void> _saveData() async {
+    if (_formKey.currentState!.validate()) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String key = DateTime.now().toString();
+
+      // Store user data as a JSON string
+      Map<String, dynamic> userData = {
+        'name': _nameController.text,
+        'email': _emailController.text,
+        'password': _passwordController.text,
+        'status': _status,
+      };
+      await prefs.setString(key, jsonEncode(userData));
+
+      Fluttertoast.showToast(
+        msg: "Data Saved Successfully",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+
+      _nameController.clear();
+      _emailController.clear();
+      _passwordController.clear();
+      setState(() {});
+    }
+  }
+
+  Widget _buildAnimatedFormField({
+    required TextEditingController controller,
+    required FocusNode focusNode,
+    required String labelText,
+    bool obscureText = false,
+    required String? Function(String?) validator,
+  }) {
+    return SlideTransition(
+      position: _slideAnimation,
+      child: FadeTransition(
+        opacity: _fadeAnimation,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: focusNode.hasFocus ? Colors.blue : Colors.grey,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: TextFormField(
+            controller: controller,
+            focusNode: focusNode,
+            obscureText: obscureText,
+            decoration: InputDecoration(
+              labelText: labelText,
+              border: InputBorder.none,
+            ),
+            validator: validator,
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculator'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _num1Controller,
-              decoration: InputDecoration(
-                labelText: 'Enter Number 1',
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              _buildAnimatedFormField(
+                controller: _nameController,
+                focusNode: FocusNode(),
+                labelText: 'Name',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
               ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _num2Controller,
-              decoration: InputDecoration(
-                labelText: 'Enter Number 2',
-                border: OutlineInputBorder(),
+              SizedBox(height: 16),
+              _buildAnimatedFormField(
+                controller: _emailController,
+                focusNode: FocusNode(),
+                labelText: 'Email',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
               ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16),
-            DropdownButton<String>(
-              value: _selectedOperation,
-              onChanged: (value) {
-                setState(() {
-                  _selectedOperation = value!;
-                });
-              },
-              items: ['Add', 'Subtract', 'Multiply', 'Divide'].map((operation) {
-                return DropdownMenuItem<String>(
-                  value: operation,
-                  child: Text(operation),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _calculate,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+              SizedBox(height: 16),
+              _buildAnimatedFormField(
+                controller: _passwordController,
+                focusNode: FocusNode(),
+                labelText: 'Password',
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
               ),
-              child: Text('Submit'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              _result,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ],
+              SizedBox(height: 16),
+              SlideTransition(
+                position: _slideAnimation,
+                child: FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: Row(
+                    children: [
+                      Text('Status:'),
+                      Radio(
+                        value: 'Active',
+                        groupValue: _status,
+                        onChanged: (value) {
+                          setState(() {
+                            _status = value.toString();
+                          });
+                        },
+                      ),
+                      Text('Active'),
+                      Radio(
+                        value: 'Inactive',
+                        groupValue: _status,
+                        onChanged: (value) {
+                          setState(() {
+                            _status = value.toString();
+                          });
+                        },
+                      ),
+                      Text('Inactive'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              SlideTransition(
+                position: _slideAnimation,
+                child: FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: ElevatedButton(
+                    onPressed: _saveData,
+                    child: Text('Register'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class DataListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          SharedPreferences prefs = snapshot.data!;
+          List<String> keys = prefs.getKeys().toList();
+          keys.sort((a, b) => b.compareTo(a)); // Sort keys in descending order
+
+          return ListView.builder(
+            itemCount: keys.length,
+            itemBuilder: (context, index) {
+              String key = keys[index];
+              String? userData = prefs.getString(key);
+
+              if (userData == null) {
+                return SizedBox.shrink(); // Skip invalid entries
+              }
+
+              // Parse the user data
+              Map<String, dynamic> user =
+                  Map<String, dynamic>.from(jsonDecode(userData));
+              String name = user['name'] ?? '';
+              String email = user['email'] ?? '';
+              String password = user['password'] ?? '';
+              String status = user['status'] ?? 'Inactive';
+
+              return AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                margin: EdgeInsets.symmetric(vertical: 4),
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color:
+                      status == 'Active' ? Colors.green[100] : Colors.red[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      status == 'Active' ? Icons.check : Icons.close,
+                      color: status == 'Active' ? Colors.green : Colors.red,
+                    ),
+                    SizedBox(width: 8),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Name: $name'),
+                        Text('Email: $email'),
+                        Text('Password: $password'),
+                        Text('Status: $status'),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
